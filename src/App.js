c8d35e88b21c2879f005b8cbcea4350ff1e13c40
zupapomidor500-byte <zupapomidor500@gmail.com>
2025-12-16 14:49:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ function App() {
         )}
       </div>
 
-      <footer className="footer">wykonal antek jaros</footer>
+      <footer className="footer">wykonal antek jaros 16.12.2025</footer>
     </div>
   );
 }
